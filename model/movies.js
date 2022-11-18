46a5cc5ed14034524263fe02d7e14e3bcc0f7939
Ochuko George <georgeochuko7@gmail.com>
2022-11-18 14:31:46 +0100

    movies api completed

--- model/movies.js
+++ model/movies.js
@@ -1,72 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const crewSchema = new Schema({
-    director: {
-        type: String,
-        require: true
-    },
-    writer: {
-        type: String,
-        require: true
-    },
-})
-const aboutMovieSchema = new Schema({
-    brief: {
-            type: String,
-            require: true
-        },
-    cast: Schema.Types.Array,
-    crew:[crewSchema],
-    rating: {
-        type: String,
-        require: true
-    },
-    theMovieIs: {
-        type: String,
-        require: true
-    },
-    length: {
-        type: String,
-        require: true
-    },
-    yearOfProduction:{
-        type: String,
-        require: true
-    },
-})
-const moviesSchema = new Schema({
-    title:{
-        type: String,
-        require: true
-    },
-    aboutMovie:[aboutMovieSchema],
-    keywords:[],
-    collections:[],
-    categories:[],
-    imageUrl: {
-        type: String,
-        require: true
-    },
-    mainVideoUrl: {
-        type: String,
-        require: true
-    },
-    thrillerVideoUrl: {
-        type: String,
-        require: true
-    },
-
-})
-// const moviesSchema = new Schema({
-//     title:{
-//         type: String,
-//         require: true
-//     },
-//     brief: {
-//         type: String,
-//         require: true
-//     },
-    
+// const crewSchema = new Schema({
 //     director: {
 //         type: String,
 //         require: true
@@ -75,6 +9,14 @@ const moviesSchema = new Schema({
 //         type: String,
 //         require: true
 //     },
+// })
+// const aboutMovieSchema = new Schema({
+//     brief: {
+//             type: String,
+//             require: true
+//         },
+//     cast: Schema.Types.Array,
+//     crew:[crewSchema],
 //     rating: {
 //         type: String,
 //         require: true
@@ -91,6 +33,16 @@ const moviesSchema = new Schema({
 //         type: String,
 //         require: true
 //     },
+// })
+// const moviesSchema = new Schema({
+//     title:{
+//         type: String,
+//         require: true
+//     },
+//     aboutMovie:[aboutMovieSchema],
+//     keywords:[],
+//     collections:[],
+//     categories:[],
 //     imageUrl: {
 //         type: String,
 //         require: true
@@ -103,23 +55,71 @@ const moviesSchema = new Schema({
 //         type: String,
 //         require: true
 //     },
-//     cast: {
-//         type: Schema.Types.Array,
-//         require: true
-//     },
-//     keywords:{
-//         type: Schema.Types.Array,
-//         require: true
-//     },
-//     collections:{
-//         type: Schema.Types.Array,
-//         require: true
-//     },
-//     categories:{
-//         type: Schema.Types.Array,
-//         require: true
-//     },
+
 // })
+const moviesSchema = new Schema({
+    title:{
+        type: String,
+        require: true
+    },
+    brief: {
+        type: String,
+        require: true
+    },
+    
+    director: {
+        type: String,
+        require: true
+    },
+    writer: {
+        type: String,
+        require: true
+    },
+    rating: {
+        type: String,
+        require: true
+    },
+    theMovieIs: {
+        type: String,
+        require: true
+    },
+    length: {
+        type: String,
+        require: true
+    },
+    yearOfProduction:{
+        type: String,
+        require: true
+    },
+    imageUrl: {
+        type: String,
+        require: true
+    },
+    mainVideoUrl: {
+        type: String,
+        require: true
+    },
+    thrillerVideoUrl: {
+        type: String,
+        require: true
+    },
+    cast: {
+        type: Schema.Types.Array,
+        require: true
+    },
+    keywords:{
+        type: Schema.Types.Array,
+        require: true
+    },
+    collections:{
+        type: Schema.Types.Array,
+        require: true
+    },
+    categories:{
+        type: Schema.Types.Array,
+        require: true
+    },
+})
 
 
 
